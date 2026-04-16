d1fc16342c59ddf1d17382dd9835c134a7a49d18
Azizbek <qodiraliyevazizbekpdp@gmail.com>
2026-04-16 15:06:46 +0500

    uptade

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Sales Data Analyzer")
+
+uploaded_file = st.file_uploader("Upload CSV")
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head())
